4e890e9cc9c68b3cd5826358e79316e4faac326a
이종현 <114001967+zzongz@users.noreply.github.com>
2022-09-21 02:37:03 +0900

    Delete Selectshop directory

--- Selectshop/src/main/java/com/sparta/week04/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.week04.repository;
-
-import com.sparta.week04.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
